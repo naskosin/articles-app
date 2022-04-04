325ec5fd0431157b9bb6889b4deb4413d6dbb2dc
Atanas Sinapov <sluckata@yahoo.com>
2022-04-04 15:04:40 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CommonModule} from '@angular/common'
 import { AppComponent } from './app.component';
 
 import { ArticleComponent } from './article/article.component';
@@ -14,7 +14,8 @@ import { ArticlesComponent } from './articles/articles.component';
   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
